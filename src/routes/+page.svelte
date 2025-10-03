3a3497a02c5c6fcc94e4a91d3663e039253b3c30
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-03 00:22:28 -0400

    svelte: add definition and explanation to page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+    .blip-definition {
+        font-weight: 600;
+        font-size: 1.4rem;
+    }
+</style>
+
+<h1 class="animated">Blips</h1>
+<article>
+    <p class="blip-definition">Blip (noun) - a radar ping</p>
+    <p>My Blips are basically my blog. Not to be confused with my <a href="https://ethmarks.github.io/posts/">Posts</a>, which are long-form and semi-professional articles. A Blip is closer to a Tumblelog than a Post. I blip about random things that I've decided that you should know but that aren't substantial enough to merit a full Post.</p>
+    <p>~Ethan</p>
+</article>
