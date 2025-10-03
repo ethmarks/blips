6cff488f14abcaa5c072aa6c4b30bd26043e65c8
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-03 00:05:17 -0400

    svelte: add ethmarks styles and components to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="stylesheet" href="https://ethmarks.github.io/css/global.min.css">
+    <script src="https://ethmarks.github.io/js/ethmarks-components.js" defer></script>
 </svelte:head>
 
+<ethmarks-header></ethmarks-header>
+<main>
 {@render children?.()}
+</main>
+<ethmarks-footer></ethmarks-footer>
