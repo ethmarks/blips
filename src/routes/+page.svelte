13a34d3a705fc3c680ede9968c41cf458dbbfa74
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-03 00:38:56 -0400

    svelte: add basic getBlips functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,3 +11,9 @@
     <p>My Blips are basically my blog. Not to be confused with my <a href="https://ethmarks.github.io/posts/">Posts</a>, which are long-form and semi-professional articles. A Blip is closer to a Tumblelog than a Post. I blip about random things that I've decided that you should know but that aren't substantial enough to merit a full Post.</p>
     <p>~Ethan</p>
 </article>
+
+<script>
+  import { getBlips } from '../lib/sanity';
+  const content = getBlips();
+  console.log(content);
+</script>
