b43c995f4dd2cb8a857d49c78863fc260b3e36da
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-03 00:46:46 -0400

    svelte: add getBlips promise resolving

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,23 @@
     }
 </style>
 
+<script>
+  import { getBlips } from '../lib/sanity';
+  let blipsPromise = getBlips();
+</script>
+
 <h1 class="animated">Blips</h1>
 <article>
     <p class="blip-definition">Blip (noun) - a radar ping</p>
     <p>My Blips are basically my blog. Not to be confused with my <a href="https://ethmarks.github.io/posts/">Posts</a>, which are long-form and semi-professional articles. A Blip is closer to a Tumblelog than a Post. I blip about random things that I've decided that you should know but that aren't substantial enough to merit a full Post.</p>
     <p>~Ethan</p>
+    <div id="blips">
+        {#await blipsPromise}
+            <p>Loading blips...</p>
+        {:then blips}
+            <p>Found {blips.length} blips!</p>
+        {:catch error}
+            <p>Error loading blips: {error.message}</p>
+        {/await}
+    </div>
 </article>
-
-<script>
-  import { getBlips } from '../lib/sanity';
-  const content = getBlips();
-  console.log(content);
-</script>
