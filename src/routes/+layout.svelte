1741181e587ba203a36b10db8e86a4267ad02ba2
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-04 15:12:37 -0400

    svelte: add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -36,7 +36,6 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href="https://ethmarks.github.io/favicon.ico" />
 	<link rel="stylesheet" href="https://ethmarks.github.io/css/global.min.css">
     <script src="https://ethmarks.github.io/js/ethmarks-components.js" defer></script>
 </svelte:head>
