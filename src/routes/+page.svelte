ea3f060f587020878b0327e80fab30b7a2f4ec05
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-05 02:23:10 -0400

    svelte: update blip definition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,12 +116,12 @@
 
 <h1 class="animated">Blips</h1>
 <article>
-    <p class="blip-definition">Blip (noun) - a radar ping</p>
+    <p class="blip-definition">Blip (noun) - a ping of activity on a radar</p>
     <p>
         My Blips are basically my blog. Not to be confused with my <a
             href="https://ethmarks.github.io/posts/">Posts</a
         >, which are long-form and semi-professional articles. A Blip is closer
-        to a Tumblelog than a Post. I blip about random things that I've decided
+        to a microblog than a Post. I blip about random things that I've decided
         that you should know but that aren't substantial enough to merit a full
         Post.
     </p>
