660a6adc017333cd3ec6955a5f09b451a7b4dfd6
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2025-10-05 00:29:00 -0400

    sanity: add sample blips

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,4 +1,5 @@
 import { createClient } from '@sanity/client';
+import { sampleBlips } from './sampleBlips'
 
 export const sanityClient = createClient({
   projectId: 'nhqqp3l1',
@@ -7,7 +8,15 @@ export const sanityClient = createClient({
   apiVersion: '2025-10-03',
 });
 
-export async function getBlips() {
+export async function getBlips(sample=false) {
+  if (sample) {
+    return await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(JSON.parse(sampleBlips).result);
+      }, 300);
+    });
+  }
+
   const blips = await sanityClient.fetch('*[_type == "blip"] | order(_createdAt desc) {_id, _createdAt, content}')
   return blips
 }
