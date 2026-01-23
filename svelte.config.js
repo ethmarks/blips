13d2ad9c183705cb54819d62eb84d767b3574900
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-22 21:38:44 -0500

    switch to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,14 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-auto";
 
 const basePath = process.env.BASE_PATH || ""; // "/blips"
-const outDir = process.env.OUT_DIR || "dist"; // "dist/blips"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: outDir,
-      assets: outDir,
-      fallback: undefined,
-      precompress: false,
-      strict: true,
-    }),
+    adapter: adapter(),
     paths: {
       base: basePath,
       relative: false,
-    },
-    prerender: {
-      handleHttpError: ({ path, referrer, message }) => {
-        // Only throw on actual errors, not 404s during prerendering
-        if (path.startsWith("/api/")) {
-          throw new Error(message);
-        }
-      },
-      entries: ["*"],
     },
   },
 };
