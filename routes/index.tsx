c28835f69480ca90186cd3311fae56fcf5c8fa20
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-02-03 14:59:54 -0500

    set up basic ethmarks design system in index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,33 +1,52 @@
-import { useSignal } from "@preact/signals";
 import { Head } from "fresh/runtime";
 import { define } from "../utils.ts";
-import Counter from "../islands/Counter.tsx";
 
-export default define.page(function Home(ctx) {
-  const count = useSignal(3);
-
-  console.log("Shared value " + ctx.state.shared);
+export default define.page(function Home() {
+  const canonUrl = "https://site-ethmarks.vercel.app/blips";
+  const title = "Blips";
+  const desc =
+    "Ethan's microblog featuring brief thoughts, activities, and interesting discoveries. A radar ping of activity.";
+  const img = "/blips.png";
 
   return (
-    <div class="px-4 py-8 mx-auto fresh-gradient min-h-screen">
+    <>
       <Head>
-        <title>Fresh counter</title>
-      </Head>
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
+        <title>Blips</title>
+        <meta name="description" content={desc} />
+
+        <link rel="canonical" href={canonUrl} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
+        <meta property="og:image" content={img} />
+        <meta property="og:url" content={canonUrl} />
+        <meta property="og:locale" content="en_US" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={desc} />
+        <meta property="twitter:image" content={img} />
+
+        <link
+          rel="icon"
+          href="https://site-ethmarks.vercel.app/common/ethmarks.ico"
+          type="image/x-icon"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
-      </div>
-    </div>
+        <link
+          rel="stylesheet"
+          href="https://site-ethmarks.vercel.app/common/ethmarks-rich.css"
+        />
+        <script
+          src="https://site-ethmarks.vercel.app/common/ethmarks.wc.js"
+          defer
+        >
+        </script>
+      </Head>
+      <eth-header active="blips"></eth-header>
+      <main>
+        <h1>Blips</h1>
+      </main>
+      <eth-footer source="https://github.com/ethmarks/blips"></eth-footer>
+    </>
   );
 });
