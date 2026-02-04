3a38ee9240bcb0abec2519e8defd4b515fa1be20
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-02-03 21:28:45 -0500

    move middleware

--- main.ts
+++ main.ts
@@ -5,7 +5,15 @@ const basePath = Deno.env.get("FRESH_BASE_PATH") || undefined; // "/blips"
 
 export const app = new App<State>({
   basePath,
-});
+})
+  .use(async (ctx) => {
+    const res = await ctx.next();
+    res.headers.set(
+      "Cache-Control",
+      "public, s-maxage=1, stale-while-revalidate=3600",
+    );
+    return res;
+  });
 
 app.use(staticFiles());
 
